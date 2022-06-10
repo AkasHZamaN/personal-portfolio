c93ae37c44814ce074b7737e7034a44e98c500ba
AkasHZamaN <akashzaman4@gmail.com>
2022-06-11 02:15:16 +0600

    footer modified

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import footerLogo from '../../portfolio-img/portfolioLogo.png';
+import facebook from '../../portfolio-img/facebook.png';
+import github from '../../portfolio-img/github.png';
+import linkedin from '../../portfolio-img/linkedin.png';
 
 const Footer = () => {
     return (
         <div className='w-100 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center items-center bg-neutral p-4 '>
             <div>
-                <img className='w-16 h-16' src={footerLogo} alt="" />
+                <img className='w-15 h-16' src={footerLogo} alt="" />
             </div>
-            <div className='text-white'>
-                <Link className='mr-4 font-bold' to={'#'}>Facebook</Link><Link className='mr-4 font-bold' to={'#'}>Instagram</Link><Link className='mr-4 font-bold' to={'#'}>in</Link>
+            <div className='text-white flex'>
+                <a href='https://www.facebook.com/akash.zaman.18400'><img className='w-8 h-8 mr-2 rounded-full' src={facebook} alt="" /></a>
+                <a href='https://github.com/AkasHZamaN'><img className='w-8 h-8 mr-2 rounded-full' src={github} alt="" /></a>
+                <a href='https://www.linkedin.com/in/md-kawser-uzzaman-4586ba230/'><img className='w-8 h-8 mr-2 rounded-full' src={linkedin} alt="" /></a>
             </div>
-            <div className='text-white font-bold'>Copyright ⓒ 2022 || allrights reserves</div>
+            <div className='text-white opacity-50 font-bold'>Copyright ⓒ 2022 || allrights reserves</div>
         </div>
     );
 };
